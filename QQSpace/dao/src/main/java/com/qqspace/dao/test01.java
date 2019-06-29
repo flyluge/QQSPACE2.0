394f99bacbc29f8e786afd616acaa1419388f9f2
chaiyunlei <c2521303550@gmail.com>
2019-06-29 16:06:26 +0800

    0

--- QQSpace/dao/src/main/java/com/qqspace/dao/test01.java
+++ QQSpace/dao/src/main/java/com/qqspace/dao/test01.java
@@ -6,21 +6,21 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.qqSpace.domain.UserVo;
+import com.qqSpace.domain.User;
 import com.qqspace.dao.base.impl.BaseDaoimpl;
 
-public class test01 extends BaseDaoimpl<UserVo>{
+public class test01 extends BaseDaoimpl<User>{
 	
-	public UserVo getUser() {
-		List<?> list = this.getHibernateTemplate().find("from UserVo");
-		return (UserVo) list.get(0);
+	public User getUser() {
+		List<?> list = this.getHibernateTemplate().find("from User");
+		return (User) list.get(0);
 	}
 	@Test
 	public void Test() {
 		@SuppressWarnings("resource")
 		ApplicationContext a=new ClassPathXmlApplicationContext("applicationContext-datasource.xml");
 		test01 t=(test01) a.getBean("test01");
-		UserVo user = t.findById(2);
+		User user = t.findById(1);
 		System.out.println(user);
 	}
 }
